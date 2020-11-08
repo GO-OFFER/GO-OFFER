1b35d9ea92d9134906c59502ddec992c7d2fa946
Juan Guillermo Romero <juan.romero-si@escuelaing.edu.co>
2020-11-08 01:45:13 -0500

    falta arreglar error por incersion en la tabla comentarios2

--- src/main/java/edu/escuelaing/eci/arsw/gooffer/model/Comentario.java
+++ src/main/java/edu/escuelaing/eci/arsw/gooffer/model/Comentario.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 public class Comentario {
 	/* falta comentario por conexion */
 	
-	@Column(name = "id")
+	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 	
